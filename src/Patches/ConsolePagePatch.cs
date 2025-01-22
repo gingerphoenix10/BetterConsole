92059b28545473e20cf0acb7da0874a877f3e737
gingerphoenix10 <gingerphoenix10@gmail.com>
2025-01-22 23:28:54 +0000

    Initial Commit

---
+++ src/Patches/ConsolePagePatch.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+using Zorro.Core.CLI;
+
+namespace BetterConsole.Patches;
+
+[HarmonyPatch(typeof(ConsolePage))]
+public class ConsolePagePatch
+{
+    private static bool pasted = false;
+    private static bool copied = false;
+    
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(ConsolePage.Update))]
+    public static void Update(ConsolePage __instance)
+    {
+        string m_currentInput = (string)typeof(ConsolePage)
+            .GetField("m_currentInput", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
+        if (UnityEngine.Input.GetKey(KeyCode.LeftControl) || UnityEngine.Input.GetKey(KeyCode.RightControl))
+        {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.V))
+            {
+                if (!pasted)
+                {
+                    typeof(ConsolePage).GetField("m_currentInput", BindingFlags.NonPublic | BindingFlags.Instance)
+                        .SetValue(__instance, m_currentInput + GUIUtility.systemCopyBuffer);
+                    pasted = true;
+                }
+            }
+            else if (pasted) pasted = false;
+            
+            if (UnityEngine.Input.GetKeyDown(KeyCode.C))
+            {
+                if (!copied)
+                {
+                    TextEditor te = new TextEditor();
+                    te.text = m_currentInput;
+                    te.SelectAll();
+                    te.Copy();
+                    copied = true;
+                }
+            }
+            else if (copied) copied = false;
+            
+        }
+    }
+}
---
+++ src/Plugin.cs
@@ -0,0 +1,15 @@
+using HarmonyLib;
+using Unity.Mathematics;
+using UnityEngine;
+using Zorro.Settings;
+
+namespace BetterConsole;
+
+[ContentWarningPlugin("BetterConsole", "0.1", vanillaCompatible: true)]
+public class BConsolePlugin
+{
+    static BConsolePlugin()
+    {
+
+    }
+}
